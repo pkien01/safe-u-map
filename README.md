7acc8b447bdb262f059447801d617af2c2269f58
pkien01 <phamkienxmas2001@gmail.com>
2022-08-06 15:02:51 -0500

    Write README and modify some stuff in the source file

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # safe-u-map
+
+This program scrapes crime locations from [UMN Safe-U Alerts](https://publicsafety.umn.edu/alerts) and display the data as an [overlaid heatmap](https://pkien01.github.io/map.html). 
+
+### How it works
+* Descriptions, as well as dates, are scraped from new Safe-U Emergency cases using BeautifulSoup.
+* Since the crime locations are not readily available, a name-entitiy recognition model (BERT NER) runs through the description and segment out phrases that identify as part of addresses. 
+* The model might not fully reliable, so the result above is sent via discord for human inspection. The user can verify whether the extracted addresses are correct based on the descriptions. 
+* All the addresses are aggregated and encoded into coordinates via Google Geocode API. These are displayed on Google Map as an overlaid heatmap showing the crime density of areas around the UMN campus. 
+* The above procedures are repeated every 5 minutes to retrieve as new emergency cases are posted. Note that the data (including dates, descriptions, locations, and whether or not it has been verified) are stored as a pkl file so that the program doesn't have to recreate the data whenever it is restarted.
+
+### How to install and use
+* Clone the repo: ```git clone https://github.com/pkien01/safe-u-map```
+* Install the necessary enviroment (recommended to do this in conda or virtualenv): ```pip install -r requirements.txt```
+* Create a discord bot by following the [instructions](https://www.freecodecamp.org/news/create-a-discord-bot-with-python/)
+* Create a new file in the cloned ```safe-u-map``` folder and name it ```.env```. Put the following details in the file
+```bash:
+BOT_TOKEN="<GET THIS FROM DISCORD>"
+API_KEY_WEB=<PLEASE REQUEST ME>
+API_KEY_LOCAL=<PLEASE REQUEST ME>
+USER_ID=<YOUR DISCORD USER ID>
+```
+* From the cloned folder's parent directory, run ```python -m safe_u```
+* Please check your discord. Assuming you have never verify any crime locations' data, you should be able to see a message like this
+![](/sample_images/discord_question.png)
+* There are two ways to reply to this
+    * If you think the extracted address(es) are correct, simply reply with ```!yes```
+    * Otherwise, reply in the format of ```!no <ADDRESS 1>; <ADDRESS 2>; ....; <ADDRESS n>```. For example, ![](/sample_images/discord_answer_no.png). Note that the addresses can be typed in any order.
+
--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import discord
 from discord.ext import tasks
-import queue
 from geopy.geocoders import GoogleV3
 from .scraper import load_old_df, scan_new_data, save_df
 from safe_u import API_KEY_LOCAL, USER_ID
 import pandas as pd
-
+import collections 
 
 class DiscordBot(discord.Client):
     async def on_ready(self):
@@ -16,7 +15,7 @@ class DiscordBot(discord.Client):
         self.df = load_old_df()
         print(self.df.dtypes)
         #print(self.df.head())
-        self.tasks_queue = queue.Queue()
+        self.tasks_queue = collections.deque()
         for _, row in self.df[~self.df['Checked']].iterrows():
             self.push((row, 'old'))
         
@@ -33,16 +32,19 @@ class DiscordBot(discord.Client):
         for _, row in df_new.iterrows():
             self.push((row, 'new'))
         #print("Queue:",list(self.tasks_queue.queue))
-        cnt = 0
-        while not self.empty() and cnt < 5: 
-            cnt += 1
+        num_tasks=len(self.tasks_queue)
+        cnt = 1
+        while not self.empty(): 
             front, which_df = self.pop()
-            notification = "%s\n%s\n\nExtracted locations:\n%s.\n\nIs this correct?" % (front['Date'].strftime("%Y-%m-%d %H:%M"), front['Content'], "\n".join(["\t\u2022 " + x[0] for x in front['Location']]))
+            notification = "[%d/%d]\t%s\n%s\n\nExtracted locations:\n%s.\n\nIs this correct?" % (cnt, num_tasks, front['Date'].strftime("%Y-%m-%d %H:%M"), front['Content'], "\n".join(["\t\u2022 " + x[0] for x in front['Location']]))
             await self.receiver.send(notification)
             msg = await self.wait_for('message', check=lambda m: m.content == "!yes" or (len(m.content) != 0 and m.content.split()[0] == "!no"))
             if msg.content == '!yes':
                 await msg.add_reaction("👍")
                 front['Checked'] = True
+                if which_df == 'old': self.df.iloc[front.name] = front
+                else: df_new.iloc[front.name] = front
+                cnt += 1
             else:
                 msg_split = msg.content.split()
                 assert len(msg_split) != 0 and msg_split[0] == "!no"
@@ -50,26 +52,30 @@ class DiscordBot(discord.Client):
                 corrected_addresses = " ".join(msg_split[1:]).split(";")
                 print("corrected_addresses:", corrected_addresses)
                 corrected_locations = []
-                for address in corrected_addresses:
-                    location = self.geolocator.geocode(address.strip() + ", Minneapolis, MN",  timeout=10)
+                for address_raw in corrected_addresses:
+                    address = address_raw.strip()
+                    if "minneapolis" not in address.lower(): 
+                        address += ", Minneapolis"
+                    if "mn" not in address.lower() and "minnesota" not in address.lower():
+                        address += ", MN"
+                    location = self.geolocator.geocode(address,  timeout=10)
                     corrected_locations.append((location.address, location.latitude, location.longitude))
                 front['Location']= corrected_locations
-                front['Checked'] = True
-            
-            if which_df == 'old': self.df.iloc[front.name] = front
-            else: df_new.iloc[front.name] = front
+                self.push_front((front, which_df))
+                #front['Checked'] = True
 
         self.df = pd.concat([df_new, self.df])
         #print(self.df.head())
         save_df(self.df)
         
     def push(self, item):
-        self.tasks_queue.put(item)
+        self.tasks_queue.append(item)
+    def push_front(self, item):
+        self.tasks_queue.appendleft(item)
     def pop(self):
-        return self.tasks_queue.get()
+        return self.tasks_queue.popleft()
     def empty(self):
-        return self.tasks_queue.empty()
-
+        return not bool(self.tasks_queue)
 '''
 if __name__ == '__main__':
     intents = discord.Intents.default()
